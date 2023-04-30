a8edee2b837e6ee44d54cf4d1d3589c6df1777e5
MaxAuMax <maxime.mallet@epitech.eu>
2023-04-30 14:34:15 +0200

    Embed in NavigationStack

--- Mediify/Mediify/LandingView.swift
+++ Mediify/Mediify/LandingView.swift
@@ -9,37 +9,48 @@ import SwiftUI
 
 struct LandingView: View {
     var body: some View {
-        VStack {
-            Text("mediify")
-                .font(.system(size: 70))
-                .foregroundColor(Color("lightPurple"))
-                .padding(.bottom, 0.5)
-
-            Text("Hi there!")
-                .font(.system(size: 22))
-                .foregroundColor(Color("darkGray"))
-            Text("Welcome to Mediify")
-                .font(.system(size: 22))
-                .foregroundColor(Color("darkGray"))
-
-            Spacer()
-
-            Image("Two Doctors")
-                .padding(.bottom)
-
-            HStack {
-                Button("Sign in") {}
+        NavigationStack {
+            VStack {
+                Text("mediify")
+                    .font(.system(size: 70))
+                    .foregroundColor(Color("lightPurple"))
+                    .padding(.bottom, 0.5)
+                
+                Text("Hi there!")
+                    .font(.system(size: 22))
+                    .foregroundColor(Color("darkGray"))
+                Text("Welcome to Mediify")
+                    .font(.system(size: 22))
+                    .foregroundColor(Color("darkGray"))
+                
+                Spacer()
+                
+                Image("Two Doctors")
+                    .padding([.bottom, .leading])
+                
+                HStack {
+                    NavigationLink {
+                        Text("Sign in page")
+                    } label: {
+                        Text("Sign in")
+                    }
                     .cornerRadius(25)
                     .padding(.trailing)
-                Button("Log in") {}
+
+                    NavigationLink {
+                        Text("Log in page")
+                    } label: {
+                        Text("Log in")
+                    }
                     .cornerRadius(25)
                     .padding(.leading)
+                }
+                .font(.system(size: 22))
+                .buttonStyle(.borderedProminent)
+                .tint(Color("lightPurple"))
             }
-            .font(.system(size: 22))
-            .buttonStyle(.borderedProminent)
-            .tint(Color("lightPurple"))
+            .padding()
         }
-        .padding()
     }
 }
 
